1bdf6b363c1787655ecf3e63c69aef9ea702f217
lruzzante <livio.ruzzante@unil.ch>
2020-10-14 16:51:12 +0200

    update

--- env/lib/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/home/lruzzant/evol-feat-snakemake/env/lib"
-XML2_LIBS="-lxml2 -L/home/lruzzant/evol-feat-snakemake/env/lib -lz -L/home/lruzzant/evol-feat-snakemake/env/lib -llzma -lpthread -L/home/lruzzant/evol-feat-snakemake/env/lib -liconv -L/home/lruzzant/evol-feat-snakemake/env/lib -licui18n -licuuc -licudata -lm "
-XML2_INCLUDEDIR="-I/home/lruzzant/evol-feat-snakemake/env/include/libxml2 -I/home/lruzzant/evol-feat-snakemake/env/include"
-MODULE_VERSION="xml2-2.9.10"
-
--- env/lib/xsltConf.sh
+++
@@ -1,7 +0,0 @@
-#
-# Configuration file for using the xslt library
-#
-XSLT_LIBDIR="-L/home/lruzzant/evol-feat-snakemake/env/lib"
-XSLT_LIBS="-lxslt -L/home/lruzzant/evol-feat-snakemake/env/lib -lxml2 -L/home/lruzzant/evol-feat-snakemake/env/lib -lz -L/home/lruzzant/evol-feat-snakemake/env/lib -llzma -lpthread -L/home/lruzzant/evol-feat-snakemake/env/lib -liconv -L/home/lruzzant/evol-feat-snakemake/env/lib -licui18n -licuuc -licudata -lm -ldl -lm -lrt"
-XSLT_INCLUDEDIR="-I/home/lruzzant/evol-feat-snakemake/env/include"
-MODULE_VERSION="xslt-1.1.33"
