23d6f61b38312a6b30faebbe07d70ddffa758f71
lruzzante <livio.ruzzante@unil.ch>
2020-10-15 17:26:16 +0200

    update

--- workflow/compute_UNI.py
+++ workflow/compute_UNI.py
@@ -1,26 +1,12 @@
-# Computing orthogroup universality (relative species-span)
-
-from collections import defaultdict
-import pandas as pd
+# Compute orthogroup universality (relative species-span)
 
 # Retrieve information from Snakemake
-input_file = open(snakemake.input[0])
-output_file_orthogroups = open(snakemake.output["UNI_orthogroups"], 'w')
-output_file_genes = open(snakemake.output["UNI_genes"], 'w')
+
+# Process files
+#for orthogroup in sorted(orthogroups.keys()):
+#    output_file_orthogroups.write(orthogroup + orthogroup['species'] + '\n')
 
 
-# Create dictionaries of orthogroups and gene features
-orthogroups = defaultdict()
-genes = defaultdict()
-
-# df = pd.read_csv(input_file, sep='\t')
-n_species = float(snakemake.config["n_species"]) #len(set(df['species']))
-
-for line in input_file:
-    orthogroup = line.split('\t')[0]
-    spec = line.split('\t')[2]
-    orthogroups[orthogroup]['species'].append(spec)
-#
-# # Process files
-# for orthogroup in sorted(orthogroups.keys()):
-#     output_file_orthogroups.write(orthogroup + orthogroup['species'] + '\n')
+input_file.close()
+output_file_orthogroups.close()
+output_file_genes.close()
