6767c10197c7adc20e5edd6db542704dd39fc376
lruzzante <livio.ruzzante@unil.ch>
2020-10-20 16:04:33 +0200

    added duplicability and generic merging rule of features to produce unique dataframe

---
+++ workflow/compute_universality.py
@@ -0,0 +1,23 @@
+# Compute orthogroup universality (relative species-span)
+
+import pickle
+
+# Retrieve information from Snakemake
+orthogroups = pickle.load(open(snakemake.input[0], 'rb'))
+output_file_orthogroups = open(snakemake.output["UNI_orthogroups"], 'w')
+output_file_genes = open(snakemake.output["UNI_genes"], 'w')
+n_species_from_config = float(snakemake.config["n_species"])
+
+# Process output files
+output_file_orthogroups.write('orthogroup' + '\t' 'UNI' + '\n')
+output_file_genes.write('gene' + '\t' 'UNI' + '\n')
+for orthogroup in sorted(orthogroups.keys()):
+    n_unique_species = len(set(orthogroups[orthogroup]["species"]))
+    UNI =  n_unique_species / n_species_from_config
+    output_file_orthogroups.write(orthogroup + '\t' + str(UNI) + '\n')
+    for gene in sorted(orthogroups[orthogroup]["genes"]):
+        output_file_genes.write(gene + '\t' + str(UNI) + '\n')
+
+# Close files
+output_file_orthogroups.close()
+output_file_genes.close()
