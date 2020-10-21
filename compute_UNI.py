7b60b4edb5f6deaf5c8fd2ae29687f5c8b2cf0ff
Livio Ruzzante <livio.ruzzante@gmail.com>
2020-10-21 17:11:13 +0200

    Delete compute_UNI.py

--- compute_UNI.py
+++
@@ -1,7 +0,0 @@
-# Retrieve information from Snakemake
-input_file = open(snakemake.input[0])
-output_file = open(snakemake.output[0], 'w')
-n_lines = len(input_file)
-
-# Process file
-output_file.write(n_lines)
