5f7206f20a3cab4eb5fe68c248f2c2220afdf8c7
lruzzante <livio.ruzzante@unil.ch>
2021-12-06 16:50:45 +0100

    modified cluster analysis script and env

--- workflow/scripts/genesets_cluster_analysis/tsne.R
+++ workflow/scripts/genesets_cluster_analysis/tsne.R
@@ -1,13 +1,19 @@
 input_features <- na.omit(read.delim(snakemake@input[[1]]))
 
 library(Rtsne)
-
-train <- input_features
+library(scales)
+library(RColorBrewer)
 
 ## Curating the database for analysis with both t-SNE and PCA
+
+n_principal_components <- 5
+pc_mean_sets <- prcomp(input_features[,-c(1,2)], center=TRUE, scale.=TRUE)$x[,1:n_principal_components]
+train <- cbind(input_features[,c(1,2)],pc_mean_sets)
 train$set<-as.factor(train$set)
 ## for plotting
-colors = rainbow(length(unique(train$set)))
+N <- length(unique(train$set))
+qual_col_pals = brewer.pal.info[brewer.pal.info$category == 'qual',]
+colors = sample(unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))[1:N])
 names(colors) = unique(train$set)
 
 ## Executing the algorithm on curated data
@@ -16,7 +22,7 @@ tsne <- Rtsne(train[,-c(1,2)], dims = 2, perplexity=30, verbose=TRUE, max_iter=5
 
 ## Plotting
 pdf(snakemake@output[[1]])
-  plot(tsne$Y, t='n', main="tsne")
-  points(tsne$Y, col=colors[train$set], cex=0.8)
-  legend(x='topright', legend=names(colors), pch=1, col=colors, cex=0.6)
+plot(tsne$Y, t='n', main="tsne")
+points(tsne$Y, col=colors[train$set], bg=alpha(colors[train$set],0.6), cex=1, pch=21)
+legend(x='topleft', legend=names(colors), pch=19, col=colors, cex=0.4)
 dev.off()
