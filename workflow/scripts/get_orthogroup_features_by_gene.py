8d3e51dd322f29ce7910b3c0add9a41b9463071e
laruzzante <livio.ruzzante@gmail.com>
2022-03-24 12:54:30 +0100

    added printout gene features¨

--- workflow/scripts/get_orthogroup_features_by_gene.py
+++ workflow/scripts/get_orthogroup_features_by_gene.py
@@ -5,3 +5,17 @@ merged_orthogroup_features = snakemake.input.merge_orthogroup_features
 orthogroups = pickle.load(open(snakemake.input.orthogroups, 'rb'))
 genes = pickle.load(open(snakemake.input.orthogroups, 'rb'))
 outfile = snakemake.output[0]
+
+with open(merged_orthogroup_features) as f, open(outfile, 'w') as f2:
+    header = f.readline()
+    new_header = 'gene\t\species\t' + header
+    f2.write(new_header)
+    next(f)
+    for line in f:
+        orthogroup = line.split('\t')[0]
+        genes = orthogroups['genes']
+        for gene in genes:
+            species = genes['species']
+            for spec in species:
+                new_line = gene + '\t' + species + '\t' + line
+                f2.write(new_line)
