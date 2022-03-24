f2806704243b2ef61923216e7572336af3d8e6ed
laruzzante <livio.ruzzante@gmail.com>
2022-03-24 13:12:09 +0100

    small fix

--- workflow/scripts/get_orthogroup_features_by_gene.py
+++ workflow/scripts/get_orthogroup_features_by_gene.py
@@ -3,14 +3,13 @@ import statistics
 
 merged_orthogroup_features = snakemake.input.merged_orthogroup_features[0]
 orthogroups = pickle.load(open(snakemake.input.orthogroups, 'rb'))
-genes = pickle.load(open(snakemake.input.orthogroups, 'rb'))
+genes = pickle.load(open(snakemake.input.genes, 'rb'))
 outfile = snakemake.output[0]
 
 with open(merged_orthogroup_features) as f, open(outfile, 'w') as f2:
     header = f.readline()
     new_header = 'gene\t\species\t' + header
     f2.write(new_header)
-    next(f)
     for line in f:
         orthogroup = line.split('\t')[0]
         genes = orthogroups[orthogroup]['genes']
